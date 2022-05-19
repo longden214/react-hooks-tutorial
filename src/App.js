e6da9184c28f4a5551a4761a337178e096dc0b51
longden <nhokimlien1411@gmail.com>
2022-05-19 17:14:38 +0700

    using the useCallback

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import {useState, useCallback} from "react"
 import './App.css';
+import Content from './Content'
 
 function App() {
+  const [count,setCount] = useState(0);
+
+  const handleIncrease = useCallback(() => {
+    setCount(prev => prev + 1)
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Content onIncrease={handleIncrease}/>
+      <h1>{count}</h1>
+      
     </div>
   );
 }
